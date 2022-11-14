0b7705a84be2c486ed0bcc4652d250497a2c200c
ferdi <ferdid047@gmail.com>
2022-11-13 21:02:09 -0800

    semangat

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,31 @@
+package com.tutorial;
+
+import com.sun.org.apache.xml.internal.security.c14n.implementations.Canonicalizer20010315;
+import jdk.internal.vm.vector.VectorSupport;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] bus = 01;
+        String[] bus = 02;
+        String[] bus = 03;
+        String[] bus = 04;
+        System.out.println("01.TopLevel");
+                System.out.println("MiddleLevel");
+                        System.out.println("RegularStaff");
+                                System.out.println("Staff");
+
+                                public static void main(String[] args){
+                                    int CEO = 1;
+                                    int J02 = 2;
+                                    int N03 = 3;
+                                    int P04 = 4;
+
+                                    System.out.println(" int + CEO + J02 + N03 + P04");
+                                    // J0268673989 NIK NASUK KE BIS 02-MIDDLE LEVEL
+            public class Array {
+                
+            }
+        }
+    }
+}
